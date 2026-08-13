ea8e7f86cdf5c421592c625315e24e41283e92bc
Thais Sanches <thaisres@live.com>
2026-08-12 23:12:57 -0300

    feat: lista11 finished

--- app/lista11/components/Painel.tsx
+++ app/lista11/components/Painel.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function Painel(){
     const[turma, setTurma] = useState<AlunoFetch[]>([]);
-    const[curso, setCurso] = useState<string>("");
+    const[curso, setCurso] = useState<string>("Todos");
 
     async function listar(){
         try {
@@ -19,24 +19,33 @@ export default function Painel(){
 
     useEffect(()=>{listar()},[]);
 
+    const lista = curso === "Todos"
+        ? turma
+        : turma.filter((aluno) => aluno.curso.toLowerCase() === curso.toLowerCase());
+
     return(
         <div className="flex flex-col gap-3 w-full">
-            <select name="curso" value={curso} onChange={(e:React.ChangeEvent<HTMLSelectElement>) => setCurso(e.target.value)} >
-                <option value="todos">Todos</option>
-                <option value="Front-end">Front-end</option>
-                <option value="Back-end">Back-end</option>
-                <option value="Mobile">Mobile</option>
-                <option value="Dados">Dados</option>
-                <option value="Design">Design</option>
-            </select>
-            {turma
-                .filter((aluno) => aluno.curso.toLowerCase() === curso.toLowerCase())
-                .map((aluno) => 
+            <div className="flex gap-4 items-center">
+                <select name="curso" value={curso} 
+                onChange={(e:React.ChangeEvent<HTMLSelectElement>) => setCurso(e.target.value)} 
+                className="border rounded p-1"
+                >
+                    <option value="Todos">Todos</option>
+                    <option value="Front-end">Front-end</option>
+                    <option value="Back-end">Back-end</option>
+                    <option value="Mobile">Mobile</option>
+                    <option value="Dados">Dados</option>
+                    <option value="Design">Design</option>
+                </select>
+                <p>✅ Presentes: {turma.filter((aluno)=>aluno.presente === true).length}</p>
+            </div>
+            <div className="flex flex-wrap gap-2 w-full">
+                {lista.map((aluno) => 
                 <div key={aluno.id} className="bg-white rounded-2xl p-4 shadow-lg">
                     <p className="font-semibold">{aluno.nome}</p>
-                    <small>{aluno.curso}</small>
-                </div>)
-            }
+                    <small>{aluno.curso} - {aluno.nota} {aluno.presente ? "✅":"❌"}</small>
+                </div>)}
+            </div>            
         </div>
     )
 }
